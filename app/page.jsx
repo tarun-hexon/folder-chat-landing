9991943735b704ed196b9d21764705eb1dfbd2be
Tarun <152253038+tarun-hexon@users.noreply.github.com>
2024-03-05 12:33:07 +0530

    add pages

--- app/page.jsx
+++ app/page.jsx
@@ -269,12 +269,12 @@ export default function Home() {
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t flex-wrap">
         <p className="text-xs">© 2024 Hexon Labs. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
+          <a className="text-xs hover:underline underline-offset-4" href="https://www.hexonlabs.com/terms-conditions" target="_blank">
             Terms of Service
-          </Link>
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
+          </a>
+          <a className="text-xs hover:underline underline-offset-4" href="https://www.hexonlabs.com/privacy-policy" target="_blank">
             Privacy
-          </Link>
+          </a>
         </nav>
       </footer>
     </div>
