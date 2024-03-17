e4db5616324e43da166037585569621aa60a3c94
Tarun <152253038+tarun-hexon@users.noreply.github.com>
2024-03-17 21:23:13 +0530

    fix nav key

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -73,7 +73,7 @@ export const FloatingNav = ({
             <span className="block sm:hidden">{navItem.icon}</span>
             <span className="sm:block lg:text-sm text-[10px]">{navItem.name}</span>
           </Link>:
-          <a href="mailto:info@hexonlabs.com" className={cn(
+          <a key={`link=${idx}`} href="mailto:info@hexonlabs.com" className={cn(
             "relative dark:text-neutral-50 items-center flex space-x-1 text-white dark:hover:text-neutral-300 hover:text-neutral-500 font-bold"
           )}><span className="block sm:hidden">{navItem.icon}</span>
           <span className="sm:block lg:text-sm text-[10px]">{navItem.name}</span></a>
